9defeb30e5b9d632168da00a3a0b3d93949a53b7
Masahiro Mark Fukui <masa@pop-os.localdomain>
2020-01-26 17:05:58 +0900

    Finished First Build

--- tk_test.py
+++ tk_test.py
@@ -1,20 +1,20 @@
 from tkinter import *
 from tkinter import filedialog
-from tkinter import messagebox
+from tkinter import messagebox, PhotoImage
 from PIL import Image, ImageTk
+import requests
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 import os
 
-CUR_PATH = os.getcwd()
-ASSET_PATH = CUR_PATH + "\\assets\\" if os.name == "nt" else CUR_PATH + "/assets/"
+# CUR_PATH = os.getcwd()
+# ASSET_PATH = CUR_PATH + "\\assets\\" if os.name == "nt" else CUR_PATH + "/assets/"
 
 class Window:
     def __init__(self, root):
         self.root = root
-        self.root.title("Test Run")
-        self.root.iconbitmap(ASSET_PATH + "logo.ico")
-
-        # self.root.wm_minsize(600, 400)
-        # self.root.grid_anchor(anchor="c")
+        self.root.title("ImageGrabber v0.101262020")
+        self.root.iconphoto(True, PhotoImage(file="./assets/logo.png"))
 
         self.image_urls = []
         self.folder = ""
@@ -28,39 +28,76 @@ class Window:
         self.message.grid(row = self.first_row, column = self.first_column,
                             sticky = "e", pady=10, padx=10)
 
-        self.entry_box = Entry(root, width=80)
+        self.entry_box = Entry(root, width=120, text="https://www.androidauthority.com/best-backgrounds-and-wallpaper-apps-for-android-653338/")
         self.entry_box.grid(row=self.first_row,
                             column=self.first_column + 1, pady=10)
 
-        self.enter_url_button = Button(text="Scrape'em", command=self.download_images)
-        self.enter_url_button.grid(
+        self.get_url_button = Button(text="Get Images", command=self.scrape_urls)
+        self.get_url_button.grid(
                             row=self.first_row, column=self.first_column + 2, sticky="w", pady=10)
         self.file_button = Button(text="Open Folder", command=self.get_folder)
         self.file_button.grid(
                             row=self.first_row, column=self.first_column + 3, sticky="w", pady=10)
 
-        self.test_button = Button(text="Test Functionality", command=self.download_images)
-        self.test_button.grid(row=self.first_row + 1,
-                            column=self.first_column + 1, padx=10)
+        self.scrape_button = Button(text="Download Images", command=self.save_images)
+        self.scrape_button.grid(
+                            row=self.first_row + 1, column=self.first_column + 2, columnspan=2, sticky="w")
 
         self.lines = 45
-        self.text = Text(root, height=self.lines, width=60)
+        self.text = Text(root, height=self.lines, width=120)
         self.text.grid(row = self.first_row + 2, column = self.first_column + 1,
                         padx=10, pady=10)
 
-    def download_images(self):
-        """
-        get all the image urls thats out there
-        """
+    def download_image(self, image_url):
+        print("Break1")
+        response = requests.get(image_url)
+        print("Break2", response.status_code)
+        filename = image_url.split("/")[-1]
+        print("Break3", filename)
+        if "." not in filename:
+            filename = filename + ".jpeg"
+        print("Break4", filename)
+        with open(f"{self.folder}/{filename}", "wb") as f:
+            f.write(response.content)
+        print("SAVED")
+
+    def save_images(self):
+        self.text.insert(END, "--Started Saving Files.....")
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.download_image, self.image_urls)
+        messagebox.showinfo(title="Operation Completed", message=f"Successfully saved {len(self.image_urls)} images")
+
+    def scrape_urls(self):
         target_url = self.entry_box.get()
+        prefix_target_url = target_url.split("/")[:3]
+        prefix_target_url = "/".join(prefix_target_url)
+        headers = {'User-Agent': 'Mozilla/5.0'}
 
-        self.text.insert(END, self.folder)
-        self.text.insert(END, "\n")
-        self.text.insert(END, target_url)
+        response = requests.get(target_url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        messagebox.showinfo(title="Job Completely Successfully",\
-                            message=f"Finished downloading {len(self.image_urls)} images")
+        images = soup.find_all("img")
+
+        for image in images:
+            if image.get("src", 0) != 0:
+                source = image["src"]
+            else:
+                source = image["data-src"]
+            self.image_urls.append(source)
         
+        self.text.insert(END, f"Scraping {target_url}...")
+
+        self.image_urls = [image for image in self.image_urls  if image.endswith("jpg") or image.endswith("png") or image.endswith("gif")]
+        self.image_urls = [prefix_target_url + image if "http" not in image else image for image in self.image_urls ]
+        self.image_urls = list(set(self.image_urls))
+        self.text.delete(1.0, END)
+        self.text.insert(END, f"Successfully scraped {len(self.image_urls)} images.")
+        self.text.insert(END, "\n" + "|")
+
+        for image in self.image_urls:
+            self.text.insert(END, "\n")
+            self.text.insert(END, f"|--{image}")
+            self.text.insert(END, "\n" + "|")
 
     def get_folder(self):
         self.folder = filedialog.askdirectory(initialdir="/")
